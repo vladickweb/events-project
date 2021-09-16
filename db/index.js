016810761c4565a85ceffb5a669d5bf5ef7b142f
Vladyslav <vladickbaraza@gmail.com>
2021-09-16 17:00:43 +0200

    wp

--- db/index.js
+++ db/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/project-2'
-
+const DB_REMOTE = process.env.DB_REMOTE
 mongoose
-	.connect(MONGO_URI)
+	.connect(DB_REMOTE)
 	.then((x) => {
 		console.log(
 			`Connected to Mongo! Database name: "${x.connections[0].name}"`
